f5e4134050f6cadbf4887ddf78acab4c33a194c9
jhkim <mfloprism@gmail.com>
2024-10-17 16:09:53 +0900

    Remove multiple folders from Git

--- flex/main.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.navbar_menu');
-const menu2 = document.querySelector('.navbar_sns');
-const toggleBtn = document.querySelector('.nav_ham');
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menu2.classList.toggle('active');
-});
